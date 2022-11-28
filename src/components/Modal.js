60f3894bd5bfb95c361b954d3bb31557893f5815
mac-37 <mac-37@Zoro.local>
2022-11-28 18:37:57 +0530

    merge code

---
+++ src/components/Modal.js
@@ -0,0 +1,82 @@
+
+import {
+  Dialog,
+  DialogContent,
+  Box,
+  makeStyles,
+} from "@material-ui/core";
+import React, { useState } from "react";
+
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const useStyles = makeStyles((theme) => ({
+  dailogOpen: {
+    borderRadius: "25px",
+    padding: "20px",
+    "& h5": {
+      color: "black",
+      fontSize: "17px",
+      paddingBottom: "1rem",
+    },
+    "& .MuiDialog-paperWidthSm": {
+      padding: "20px",
+    },
+  },
+}));
+const Item = styled(motion.div)`
+  width: 20rem;
+  margin-right: 6rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100%;
+    height: auto;
+    cursor: pointer;
+  }
+  h1 {
+    display: inline-block;
+    width: fit-content;
+    font-weight: 500;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  @media (max-width: 48em) {
+    width: 15rem;
+  }
+`;
+
+export default function Modal({ openDeleteModal, setOpenDeleteModal, content }) {
+  const classes = useStyles();
+  console.log("content--", content);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("");
+  console.log("status---", status);
+  return (
+    <Dialog
+      className={classes.dailogOpen}
+      open={openDeleteModal}
+      maxWidth="sm"
+      fullWidth
+      onClose={() => setOpenDeleteModal(false)}
+    >
+      <DialogContent>
+      <Item
+        
+        initial={{ filter: "grayscale(100%)" }}
+        whileInView={{ filter: "grayscale(0%)" }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: false, amount: "all" }}
+      >
+        <img src={content} alt={content}/>
+      </Item>
+      </DialogContent>
+      <Box align="center" style={{ marginLeft: "10px", padding: "5px 20px" }}>
+      </Box>
+    </Dialog>
+  );
+}
